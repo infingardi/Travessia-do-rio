18f2968c0e8ded79524f0ac24e49b4101456df48
Leonorico <leonorico.borges@levty.com>
2025-03-11 16:31:09 -0300

    reparando arvore A Estrela

--- main.py
+++ main.py
@@ -2,19 +2,23 @@ from busca.backtracking  import solve_backtracking
 from busca.largura  import solve_bfs
 from busca.profundidade  import solve_dfs
 from busca.helpers import export_tree
+from busca.a_estrela import solve_a_star
 
 from anytree import Node
 
 
 if __name__ == "__main__":
-    solution, root = solve_backtracking()
-    export_tree(root, "backtrackingTree")
+    # solution, root = solve_backtracking()
+    # export_tree(root, "backtrackingTree")
 
-    solution, root = solve_bfs()
-    export_tree(root, "larguraTree")
+    # solution, root = solve_bfs()
+    # export_tree(root, "larguraTree")
 
-    solution, root = solve_dfs()
-    export_tree(root, "profundidadeTree")
+    # solution, root = solve_dfs()
+    # export_tree(root, "profundidadeTree")
+
+    solution, root = solve_a_star()
+    export_tree(root, "aEstrelaTree")
 
     # if solution:
     #     print("Solução encontrada:")
